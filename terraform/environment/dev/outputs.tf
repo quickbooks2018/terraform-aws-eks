4206763ae7d6adad36f2bec4f78efcc7e95eb1c6
Muhammad Asim <36363841+quickbooks2018@users.noreply.github.com>
2022-06-16 11:45:48 +0500

    Update outputs.tf

--- terraform/environment/dev/outputs.tf
+++ terraform/environment/dev/outputs.tf
@@ -2,6 +2,6 @@ output "vpc" {
   value = module.eks_vpc
 }
 
-output "karpenter_controller_iam_role" {
-  value = module.karpenter_controller_iam_role.iam_assumable_role_karpenter_iam_role_arn
+output "eks" {
+  value = module.eks
 }
